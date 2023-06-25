8519ac632c1dd4357a0fc69f0b1bdffaa03d3201
LWlodek <wlodeksn@gmail.com>
2023-06-25 15:14:37 +0200

    poprawienie wyswietlania daty wypozyczenia

--- base/models.py
+++ base/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import timedelta, datetime
+import pytz
 
 
 class Book(models.Model):
@@ -13,12 +14,12 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-# czas pokazuje ze strefy innej niż Polska, do skorygowania, dodany klucz identyfikacyjny
+
 class BorrowedBook(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='books_borrowed')
     book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True, related_name='borrowed_books')
-    borrowed_date = models.DateTimeField(auto_now_add=True)
+    borrowed_date = models.DateTimeField(default=datetime.now())
     return_to_date = models.DateTimeField(default=datetime.now() + timedelta(days=7))
     returned_date = models.DateTimeField(null=True, blank=True)
 
